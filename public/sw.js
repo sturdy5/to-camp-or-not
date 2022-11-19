7af78c54f28bfb4966b74e8fab8726c731a4805e
sturdy5 <1645884+sturdy5@users.noreply.github.com>
2022-11-18 22:01:36 -0500

    Trying to get the PWA to work

--- public/sw.js
+++ public/sw.js
@@ -5,4 +5,17 @@ importScripts(
 // This is your Service Worker, you can put any of your custom Service Worker
 // code in this file, above the `precacheAndRoute` line.
 
+
+// Change this to your repository name
+var GHPATH = '/to-camp-or-not';
+
+// Choose a different app prefix name
+var APP_PREFIX = 'tcontc_';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…).
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
