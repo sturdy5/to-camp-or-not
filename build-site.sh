bc6cdd4a158cd9dd2efcd3f72ffaaa20783e169b
sturdy5 <1645884+sturdy5@users.noreply.github.com>
2022-11-18 21:50:42 -0500

    Add script to help build the site

---
+++ build-site.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd docs
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+cd -
